3e6b9662f0d1a9b6b04a6ad046f58c12dd262b88
Petuuuhhh <peterswanton191@bellsouth.net>
2024-05-27 18:57:54 -0400

    JSON progress!

--- HMA_Python_Scripts.py
+++ HMA_Python_Scripts.py
@@ -1,6 +1,80 @@
-output = open("output.txt", "w") 
+# output = open("output.txt", "w") 
 #for mon in data.pokemon.stats:
   # output.write('+"' + mon.speciesname + '"\n')
-for item in data.items.info:
-  output.write('+"' + item.name + '"\n')
-output.close()
+# for item in data.items.info:
+  # output.write('+"' + item.name + '"\n')
+import json
+output = {}
+mons = []
+keys = [
+    'hp',
+    'attack',
+    'defense',
+    'speed',
+    'spatk',
+    'spdef',
+    'type1',
+    'type2',
+    'catchrate',
+    'padding',
+    'expyield',
+    # 'evs',
+    'itemcommon',
+    'itemrare',
+    'genderratio',
+    'eggcycles',
+    'friendship',
+    'growthrate',
+    'egggroup1',
+    'egggroup2',
+    'ability1',
+    'ability2',
+    'hiddenability',
+    'safarizonefleerate',
+    'cryid',
+    'natdexnum',
+    'height',
+    'weight',
+    'pokemonscale',
+    'pokemonoffset',
+    'trainerscale',
+    'traineroffset',
+    'padding',
+    'description',
+    # 'graphics',
+    'frontanimdelay',
+    'frontanimid',
+    'backanimid',
+    'frontpicsize',
+    'frontpicsizefemale',
+    'frontpicyoffset',
+    'backpicsize',
+    'backpicsizefemale',
+    'backpicyoffset',
+    # 'iconpal',
+    'enemymonelevation',
+    # 'flags',
+    # 'flags2',
+    # 'leveluplearnset',
+    # 'teachablelearnset',
+    # 'evolutions',
+    # 'formchangetable',
+]
+def method(i):
+    exec_scope = {}
+    exec("method = 'method" + str(i) + "'", exec_scope)
+    return exec_scope['method']
+for mon in data.pokemon.info:
+  speciesname = mon.speciesname
+  if speciesname not in mons:
+    output[speciesname] = {}
+    for key in keys:
+      output[speciesname][key] = mon[key]
+  else:
+    output[speciesname + str(mons.count(speciesname) + 1)] = {}
+    for key in keys:
+      output[speciesname + str(mons.count(speciesname) + 1)][key] = mon[key]
+  mons.append(speciesname)
+output = json.dumps(output, indent=4)
+with open("output.json", "w") as outfile:
+    outfile.write(output)
